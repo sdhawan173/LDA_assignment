d55a6f0223a49ebe68f521914b63dcafaf6ae832
Sahil <sdhawan173@pm.me>
2024-02-16 21:56:57 -0500

    final (for now) revisions

--- text_analysis.py
+++ text_analysis.py
@@ -1,4 +1,23 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+def collect_all_words(text_split_word, exclusion=False):
+    """
+    collects all words from text_split_word or all_comments_lemmatized into one array of words
+    :param text_split_word:
+    :param exclusion: Boolean to exclude STOP_WORDS and PUNCTUATION
+    :return: list of all words
+    """
+    insert = ''
+    if exclusion is True:
+        insert = ', excluding stop words and punctuation,\n'
+    print('\nCollecting all words{} in {} comments into one list ...'.format(insert, len(text_split_word)))
+    all_words = []
+    for comment in text_split_word:
+        for word in comment:
+            all_words.append(word)
+    return all_words
 
 
 def unique(all_words):
@@ -34,3 +53,12 @@ def average_words_per_comment(comment_word_list, exclusion=False):
     average_tokens_stat = sum(average_tokens_num_list) / len(comment_word_list)
     print(average_tokens_stat)
     return average_tokens_stat
+
+
+def plot_lda_distribution(lda_model):
+    output_string = lda_model.print_topics()
+    topic_num = len(output_string)
+    print(topic_num)
+    for output in output_string:
+        print(output)
+    return
